cc8d9d6ea3d077596aa2954c08fcce8fb0efc06d
Manuel Bogner <manuel@coffeebeans.at>
2018-07-18 02:52:07 +0200

    add sdk for java

--- install-base.sh
+++ install-base.sh
@@ -53,5 +53,16 @@ sudo usermod -aG docker manuel
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+# sdkman
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+sdk update
+sdk upgrade
+sdk install java
+sdk install gradle
+sdk install maven
+sdk install visualvm
+
 ###############################################################################
 echo "done"
